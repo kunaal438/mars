0475d7b815fb858431b9214fea15c5b8f06dedb2
kunaal438 <kunaal438@gmail.com>
2020-09-30 20:25:13 +0530

    work done

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const allTemp = [...document.querySelectorAll('.temp')];
-const numerals = [...document.querySelectorAll('.card h2')];
-const pressure = [...document.querySelectorAll('.pressure')];
-const speed = [...document.querySelectorAll('.speed')];
-
-let a;
-
 fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
 .then(res => res.json())
 .then(data => {
-    console.log(data)
-    a = data;
+    enterTheData(data);
 });
+
+const enterTheData = (data) => {
+    const numerals = [...document.querySelectorAll('.card h2')];
+    const pressure = [...document.querySelectorAll('.pressure')];
+    const speed = [...document.querySelectorAll('.speed')];
+    data.sol_keys.map((item, index) => {
+        numerals[index].innerHTML = `${Math.floor((data[item].AT.av - 32) * 5/9)} C`;
+        pressure[index].innerHTML = `AP : ${data[item].PRE.av} pa`;
+        speed[index].innerHTML = `HWS : ${data[item].HWS.av} m/s`;
+    })
+}
