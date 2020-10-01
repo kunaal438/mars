763231d599b05115f361279d8fc5ee23d09bf5bc
kunaal438 <kunaal438@gmail.com>
2020-10-01 18:10:12 +0530

    added my api key

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
+fetch('https://api.nasa.gov/insight_weather/?api_key=hD5PtuNYT8xv9g5S6kCgu30rPBFoHnaXvQGVWyXD&feedtype=json&ver=1.0')
 .then(res => res.json())
 .then(data => {
     console.log(data);
