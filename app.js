cd140006981105c0f4891affe021e38c8e167a31
kunaal438 <kunaal438@gmail.com>
2020-10-01 18:09:13 +0530

    made responsive for mobiles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
 .then(res => res.json())
 .then(data => {
+    console.log(data);
     enterTheData(data);
 });
 
